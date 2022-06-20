ee0873057187d5c0d397c894373708fa1f652e06
Jayce0918 <51176006+Jayce0918@users.noreply.github.com>
2022-06-21 01:29:55 +0800

    2022-06-21 首版

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,93 @@
+﻿using FireSharp;
+using FireSharp.Config;
+using FireSharp.Interfaces;
+using FireSharp.Response;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using THU_FORM.Models;
+
+namespace THU_FORM.Controllers
+{
+    public class HomeController : Controller
+    {
+        readonly IFirebaseClient client;
+        public HomeController()
+        {
+            IFirebaseConfig config = new FirebaseConfig
+            {
+                AuthSecret = "TszB5Hmop1Jb64FoePa2ITKEbYmL39ZzNavOVQvA",
+                BasePath = "https://thu-form-default-rtdb.asia-southeast1.firebasedatabase.app/"
+            };
+
+            client = new FirebaseClient(config);
+        }
+        public ActionResult Index()
+        {
+            // Dictionary<string, SignUpList> list = new Dictionary<string, SignUpList>();
+            FirebaseResponse response = client.Get("contact");
+            dynamic data = JsonConvert.DeserializeObject<dynamic>(response.Body);
+            Dictionary<string, dynamic> result = data.ToObject<Dictionary<string, dynamic>>();
+            List<SignUpList> signUpList = new List<SignUpList>();
+            foreach (KeyValuePair<string, dynamic> element in result)
+            { 
+                signUpList.Add(new SignUpList()
+                {
+                    TH = element.Value.TH,
+                    Name = element.Value.Name,
+                    Mail = element.Value.Mail,
+                    PeopleNumber = element.Value.PeopleNumber,
+                    WantToSay = element.Value.WantToSay
+                });
+
+            }
+
+            return View(signUpList);
+        }
+
+        public ActionResult Theme()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Contact(SignUpList signUpList)
+        {
+            string Id = Guid.NewGuid().ToString("N");
+            SetResponse response = client.Set("contact/" + Id, signUpList);
+
+            //if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            //{
+            //    TempData["Message"] = "報名成功 期待您的蒞臨 <3";
+            //    return RedirectToAction("Contact");
+            //}
+            //else
+            //{
+            //    TempData["Message"] = "報名失敗 請洽系統管理員 : leekuantean@gmail.com";
+            return RedirectToAction("Contact");
+            //}
+        }
+
+        public ActionResult PageNotFound()
+        {
+            return View();
+        }
+        public ActionResult Edit()
+        {
+            return View();
+        }
+
+        public ActionResult Delete()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/SignUpList.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace THU_FORM.Models
+{
+    public class SignUpList
+    {
+        //第幾屆
+        public string TH { get; set; }
+
+        //姓名
+        public string Name { get; set; }
+
+        //信箱
+        public string Mail { get; set; }
+
+        //攜伴人數
+        public string PeopleNumber { get; set; }
+
+        //想說的話
+        public string WantToSay { get; set; }
+
+        //確定參加
+        public byte ConfirmOK { get; set; }
+    }
+}
